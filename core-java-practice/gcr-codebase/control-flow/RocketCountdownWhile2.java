c6802d5419704c833188e9daacc347f4201b8ec6
Arman11r <arman0078627@gmail.com>
2025-12-19 15:58:12 +0530

    [Arman Ahmed] Add.added level 2 Q8 in control-flow

---
+++ core-java-practice/gcr-codebase/control-flow/RocketCountdownWhile2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class RocketCountdownWhile2 {
+
+    public static void main(String[] args) {
+
+        // scanner for input
+
+        Scanner sc = new Scanner(System.in);
+
+        // counter valuess
+
+        int counter;
+
+        // asking start number
+
+        System.out.print("Enter countdown for start numbers :  ");
+        counter = sc.nextInt();
+
+        // loop for countdown
+
+        while (counter >= 1) {
+            System.out.println(counter );
+            counter--;
+        }
+    }
+}
