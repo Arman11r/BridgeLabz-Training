341340e5b8b2ece29ea3b652183499c3b742a4d8
Arman11r <arman0078627@gmail.com>
2025-12-23 12:34:39 +0530

    [Arman Ahmed] Add.added employee.java question in opps/gcr-codebase/java-class-and-methods

---
+++ oops/gcr-codebase/java-class-and-objects/Employee.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.oops.levelone;
+
+class Employee {
+
+    // storing employee data
+    private String name;
+    private int id;
+    private double salary;
+
+    // constructor for setting values
+    Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    // showing employee details
+    void displayEmployee() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee id: " +id);
+        System.out.println("Employee Salary: " + salary);
+    }
+    // main method
+    public static void main(String[] args) {
+
+        // object creation
+        Employee emp = new Employee("Rohan", 1, 500000);
+
+        // calling display
+        emp.displayEmployee();
+    }
+}
